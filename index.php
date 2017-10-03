b0449cf0a5a4378719a62fab181a1c4f096bdd02
Синдикаева <Antoshka95>
2017-10-03 14:40:57 +0700

    замена

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+$price=[120,130,150,130,180];
+$prices [0]=50; $prices[]=200;
+$sum =0;
+$avg=0;
+For ($i=0; $i<count($price);$i++ ) {
+$sum+=$price [$i];}
+		
+			$avg= $sum/count($price);
+			echo $avg;
+?>
